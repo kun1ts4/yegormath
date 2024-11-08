709d4b6d6ad6188e2440ce3c7738fbae7361670c
Yegor <mendalhornet@gmail.com>
2024-11-08 16:20:22 +0300

    added Matrix struct

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 
 func main() {
 	// Main function initializes the matrix and prints its determinant.
-	matrix := [][]int{
-		{3, -3, -5, 8},
-		{-3, 2, 4, -6},
-		{2, -5, -7, 5},
-		{-4, 3, 5, -6},
+	matrix := Matrix{
+		numbers: [][]int{
+			{3, -3, -5, 8},
+			{-3, 2, 4, -6},
+			{2, -5, -7, 5},
+			{-4, 3, 5, -6},
+		},
+		size: 4,
 	}
 	fmt.Println(Determinant(matrix))
 }
